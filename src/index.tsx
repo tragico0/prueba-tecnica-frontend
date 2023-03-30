67777899f7d89840a5e0825169a0a312d98ddcf6
Victor Martinez <vic.mtz96@hotmail.com>
2023-03-30 10:01:29 -0600

    Add create employee page

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import RootLayout from './routers/RootLayout';
+import RootLayout from './routes/RootLayout';
+import CreateEmployee from './routes/CreateEmployee';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
-
+    
 const router = createBrowserRouter([
-  {path: '/', element: <RootLayout />}
+    {path: '/', element: <RootLayout />, children: [
+        {path: 'employees', element: null, children: [
+            {path: 'create', element: <CreateEmployee />}
+        ]}
+    ]}
 ]);
 
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
