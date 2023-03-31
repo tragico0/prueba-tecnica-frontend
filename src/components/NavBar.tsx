9326bc8ebce4910beb844a2aae667938aa6ba310
Victor Martinez <vic.mtz96@hotmail.com>
2023-03-31 15:47:42 -0600

    Add navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+export default function () {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Rinku</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink  to="/employees/create" className="nav-link">Crear Empleado</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/employees/create-movement" className="nav-link">Capturar Movimiento</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/payroll/monthly" className="nav-link">Movimientos Por Mes</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
 export default function RootLayout () {
     return (
         <div className="root-layout">
+            <NavBar />
             <Outlet />
         </div>
     );
